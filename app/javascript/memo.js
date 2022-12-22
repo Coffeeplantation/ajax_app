a501452f4661e76bdab065f17c2ffc8e824fce5e
Coffeeplantation <118273075+Coffeeplantation@users.noreply.github.com>
2022-12-22 20:52:01 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,13 +1,14 @@
 function post (){
   const submit = document.getElementById("submit");
 submit.addEventListener("click", (e) => {
+  console.log("イベント発火");
   e.preventDefault();
 const form = document.getElementById("form");
 const formData = new FormData(form);
 const XHR = new XMLHttpRequest();
 XHR.open("POST", "/posts", true);
 XHR.responseType = "json";
-XHR.send(formData)
+XHR.send(formData);
 });
 };
 
